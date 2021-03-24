3057a60e850264a0f4e51f7de69cb0c90c3055ba
Karim <m.karimharras@gmail.com>
2021-03-24 15:56:14 +0100

    Define routes, links Update ProductListComponent to read category id param Update ProductService to call the new URL

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {map} from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Product } from '../common/product';
 
 @Injectable({
@@ -9,18 +9,25 @@ import { Product } from '../common/product';
 })
 export class ProductService {
 
-  private baseUrl='http://localhost:8080/api/products';
+  private baseUrl = 'http://localhost:8080/api/products';
 
-  constructor(private httpClient : HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  getProductList():Observable<Product[]>{
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  getProductList(theCategoryId: number): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
+
+  // getProductList():Observable<Product[]>{
+  //   return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+  //     map(response => response._embedded.products)
+  //   );
+  // }
 }
-interface GetResponse{
-  _embedded:{
-    products:Product[];
+interface GetResponse {
+  _embedded: {
+    products: Product[];
   }
 }
