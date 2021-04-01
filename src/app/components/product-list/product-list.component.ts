e007136fe0510ab4ad895749791a6675a8e7e2df
Karim <m.karimharras@gmail.com>
2021-04-01 09:58:56 +0200

    Creation of Search Component update productList Component to search for products Update ProductService to call the URL of searching products by keyword

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -14,6 +14,7 @@ export class ProductListComponent implements OnInit {
   products: Product[];
   currentCategoryId: number;
   currentCategoryName: string;
+  searchMode: boolean;
 
 
   constructor(private productService: ProductService, private route: ActivatedRoute) { }
@@ -26,6 +27,27 @@ export class ProductListComponent implements OnInit {
   }
 
   listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    }
+    else {
+      this.handleListProducts();
+    }
+  }
+
+  handleSearchProducts(){
+    const theKeyword :string = this.route.snapshot.paramMap.get('keyword');
+
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
+
+  handleListProducts() {
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
     if (hasCategoryId) {
@@ -43,6 +65,4 @@ export class ProductListComponent implements OnInit {
       }
     );
   }
-
-
 }
