5504a4252c513a2dc5c2a7d1bc45c85327fdc777
Karim <m.karimharras@gmail.com>
2021-04-13 10:31:56 +0200

    Creation of CartStatus Component, CartService and CartItem model class Enhance CartStatus Component to subscribe to CartService Update ProductList Component and ProductDetails Component to handle Add to cart button

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
 import { ProductCategory } from 'src/app/common/product-category';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -25,7 +27,7 @@ export class ProductListComponent implements OnInit {
 
 
 
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  constructor(private productService: ProductService, private route: ActivatedRoute, private cartService: CartService) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
@@ -102,5 +104,11 @@ export class ProductListComponent implements OnInit {
     this.thePageSize = pageSize;
     this.thePageNumber = 1;
     this.listProducts();
+  }
+
+  addToCart(theProduct: Product) {
+    console.log(`Adding to cart : ${theProduct.name}, ${theProduct.unitPrice}`);
+    const theCartItem = new CartItem(theProduct);
+    this.cartService.addToCart(theCartItem);
   }
 }
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CartItem } from '../common/cart-item';
+import { Product } from '../common/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() { }
+
+
+  addToCart(theCartItem: CartItem) {
+
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem = undefined;
+
+    if (this.cartItems.length > 0) {
+      
+      existingCartItem = this.cartItems.find(tempCartItem=>tempCartItem.id === theCartItem.id);
+
+      alreadyExistsInCart = (existingCartItem != undefined);
+    }
+    
+    if (alreadyExistsInCart) {
+      existingCartItem.quantity++;
+    }
+    else {
+      this.cartItems.push(theCartItem);
+    }
+
+    this.computeCartTotals();
+  }
+
+  computeCartTotals() {
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for (let tempCartItem of this.cartItems) {
+      totalPriceValue += tempCartItem.unitPrice * tempCartItem.quantity;
+      totalQuantityValue += tempCartItem.quantity;
+    }
+
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+  }
+}
